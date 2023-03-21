a96d887f73aa3e59de81569880c959d1e88492d9
scotuzzijr <marcio@up4tech.com.br>
2023-03-21 10:15:35 -0300

    feat: implementing recursive fibonacci algorithm and testing both algorithms

--- fibonacci/fibonacci.go
+++ fibonacci/fibonacci.go
@@ -11,3 +11,11 @@ func Fibonacci(n int) int {
 	} 
 	return firstElement
 }
+
+func FibonacciRecursive(n int) int {
+	if n <= 1 {
+		return n
+	} 
+
+	return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2)
+}
--- fibonacci/fibonacci_test.go
+++ fibonacci/fibonacci_test.go
@@ -30,3 +30,30 @@ func TestFibonacci(t *testing.T) {
 		}
 	}
 }
+
+func TestFibonacciRecursive(t *testing.T) {
+	tests := []struct{
+		nthTerm 	  int
+		expectedValue int
+	}{
+		{0, 0},
+		{1, 1},
+		{2, 1},
+		{3, 2},
+		{4, 3},
+		{5, 5},
+		{6, 8},
+		{7, 13},
+		{8, 21},
+		{9, 34},
+		{10, 55},
+		{11, 89},
+		{12, 144},
+	}
+
+	for _, test := range tests {
+		if got := FibonacciRecursive(test.nthTerm); got != test.expectedValue {
+			t.Errorf("expected %v, got %v", test.expectedValue, got)
+		}
+	}
+}
